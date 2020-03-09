e1f6ccf9028d8b737eafca20aa8ea59610886d58
rborges89 <ramon.vidala89@gmail.com>
2020-03-09 12:50:43 +0100

    authentication features implementent

---
+++ src/EventSubscriber/RequestSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+// api/src/EventSubscriber/BookMailSubscriber.php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Exception\AppUnauthorizedHttpException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+use App\Service\RedisCacheService;
+
+final class RequestSubscriber implements EventSubscriberInterface
+{
+
+
+    private $redis;
+
+    public function __construct(RedisCacheService $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['checkRequestIntegrity', EventPriorities::PRE_READ],
+        ];
+    }
+
+    public function decodeJWT($jwtToken): array
+    {
+        $decoder = new JsonDecode();
+        $jwtDecoded = $decoder->decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', $jwtToken)[1]))), 'json');
+        return get_object_vars($jwtDecoded);
+    }
+
+    public function checkRequestIntegrity(RequestEvent $event): void
+    {
+
+        $headers = $event->getRequest()->headers;
+        $authHeader = $headers->get('authorization');
+        if (!$authHeader) {
+            $unauthorizedException = new UnauthorizedHttpException('Auth', 'Unauthorized error, please provide valid JWT');
+            $msg = $unauthorizedException->getMessage();
+            $statusCode = $unauthorizedException->getStatusCode();
+            $event->setResponse(new JsonResponse(array('code' => $statusCode, 'message' => $msg)));
+        } else {
+            $jwt = explode(" ", $authHeader)[1];
+            $decodedJwt = $this->decodeJWT($jwt);
+            $username = $decodedJwt['username'];
+            //comparar token jwt con cache redis en el mismo user
+            //echo $this->redis->get($username);
+            //echo var_dump($username); exit;
+        }
+
+    }
+}
